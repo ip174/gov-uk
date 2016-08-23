64546c182f5ffa9881e07d4d00c660aa688f07b2
Ion <nanu_rtc@yahoo.com>
2016-08-23 15:07:21 +0100

    Fix small errors on loading the JSON data

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
+var objJSON = "js/data.json";
 $(window).load(function(){
 	getData();
 	search();	
 	displayAllInfo(getParameterByName('uid'));	
-}); //end
+}); 
 
 //get data from JSON
 function getData(){
-	//get and append data to table
-	$.getJSON( "data.json", function( data ) {
-	for (var i=0; i<=data.length; i++){
-		
-		$("#applicants_tables tbody").append('<tr onclick="redirect('+data[i].userID+')"><td>'+data[i].firstName+
-			'</td><td>'+data[i].lastName+
-			'</td><td>'+data[i].email+
-			'</td><td>'+data[i].phone+
-			'</td><td>'+data[i].previous_role+
-			'</td><td>'+data[i].date_applied+'</td></tr>'
-		); 
-	  }
+	$.getJSON(objJSON, function( data ) {
+		for (var i=0; i<=data.length; i++){
+			$("#applicants_tables tbody").append('<tr onclick="redirect('+data[i].userID+')"><td>'+data[i].firstName+
+				'</td><td>'+data[i].lastName+
+				'</td><td>'+data[i].email+
+				'</td><td>'+data[i].phone+
+				'</td><td>'+data[i].previous_role+
+				'</td><td>'+data[i].date_applied+'</td></tr>'
+			); 
+		}
 	});		
 }
 
 //search function
 function search(){
 	//search by name or phone
-	$('#search').keyup(function() {//when key is pressed in search bar
-	var searchTerm = $(this).val(); //val of search bar
-	var myExp = new RegExp(searchTerm, "i"); //regular experation
-	$.getJSON('data.json', function(data){//get the json file
+	$('#search').keyup(function() {
+	var searchTerm = $(this).val(); 
+	var myExp = new RegExp(searchTerm, "i"); 
+	$.getJSON(objJSON, function(data){
 		var output = "<ul id='result'>";
 		$.each(data, function(key, val){
 			if(((val.phone.search(myExp) != -1) || (val.firstName.search(myExp) != -1) || (val.lastName.search(myExp) != -1)) && searchTerm != ""){
@@ -35,7 +34,7 @@ function search(){
 				output += '<a href="#" onclick="redirect('+val.userID+')">' +val.firstName+ ' '+val.lastName+'</a>';
 				output += '</li>';
 		   }
-		});//end each
+		});
 		output += "</ul>";
 		$('#update').html(output);//output result to the update div
 		});
@@ -45,7 +44,7 @@ function search(){
 //display all the info on profile page
 function displayAllInfo(userID){
 	//alert(userID);
-	$.getJSON( "data.json", function( data ) {
+	$.getJSON(objJSON, function( data ) {
 		for (var i=0; i<=data.length; i++){
 			if(data[i].userID == userID){
 				$('.display').append('<tr class="name"><td><h2>'+data[i].firstName+' '+data[i].lastName+'</h2></td></tr>'+
